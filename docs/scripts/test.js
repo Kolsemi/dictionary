cc1a2be71c58b58faaf85939d8ff2341c110db2c
Kolsemi <melkor5456@gmail.com>
2022-02-06 09:45:06 -0500

    fgit

--- docs/scripts/test.js
+++ docs/scripts/test.js
@@ -2,7 +2,7 @@ const myHeading = document.querySelector('h1');
 console.log(myHeading);
 
 async function getCSV() {
-    const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+    const requestURL = 'https://github.com/Kolsemi/dictionary/blob/main/dictionary.csv';
     const request = new Request(requestURL);
 
     const response = await fetch(request);
