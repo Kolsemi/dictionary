ee24e73de8baccbb8f06e6e2b9b3a744122b2785
Kolsemi <melkor5456@gmail.com>
2022-02-05 19:12:38 -0500

    fgit

--- docs/scripts/test.js
+++ docs/scripts/test.js
@@ -1,2 +1,8 @@
 const myHeading = document.querySelector('h1');
 console.log(myHeading);
+
+const reader = new FileReader();
+reader.addEventListener('load', (event) => {
+    img.src = event.target.result;
+    });
+reader.readAsDataURL(file);
