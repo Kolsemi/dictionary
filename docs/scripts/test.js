2528a1529afa63e1e65d0f465c8f6060dd4cb2d8
Kolsemi <melkor5456@gmail.com>
2022-02-06 09:38:44 -0500

    ls

--- docs/scripts/test.js
+++ docs/scripts/test.js
@@ -1,7 +1,7 @@
 const myHeading = document.querySelector('h1');
 console.log(myHeading);
 
-asyn function getCSV() {
+async function getCSV() {
     const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
     const request = new Request(requestURL);
 
