3261a9d3808110e5b3573354b3bb0bf192f32429
Kolsemi <melkor5456@gmail.com>
2022-02-06 09:32:25 -0500

    //

--- docs/scripts/test.js
+++ docs/scripts/test.js
@@ -1,9 +1,10 @@
 const myHeading = document.querySelector('h1');
 console.log(myHeading);
 
-const reader = new FileReader();
-reader.addEventListener('load', (event) => {
-    img.src = event.target.result;
-    });
-reader.readAsDataURL('dictionary.csv');
+asyn function getCSV() {
+    const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+    const request = new Request(requestURL);
+
+    const response = await fetch(request);
+}
 
