6678ed5d9a45217426bf2271e602d037d0109230
Kolsemi <melkor5456@gmail.com>
2022-02-06 11:36:20 -0500

    fgit

--- docs/scripts/test.js
+++ docs/scripts/test.js
@@ -1,10 +1,25 @@
 const myHeading = document.querySelector('h1');
 console.log(myHeading);
+async function getTextFromStream(readableStream) {
+    let reader = readableStream.getReader();
+    let utf8Decoder = new TextDecoder();
+    let nextChunk;
+    
+    let resultStr = '';
+    
+    while (!(nextChunk = await reader.read()).done) {
+        let partialData = nextChunk.value;
+        resultStr += utf8Decoder.decode(partialData);
+    }
+    
+    return resultStr;
+}
 
 async function getCSV() {
     const requestURL = 'https://github.com/Kolsemi/dictionary/blob/main/dictionary.csv';
     const request = new Request(requestURL);
     const response = await fetch(request, {mode: 'no-cors', headers: {'Content-Type': 'text/plain'}});
-    return response
+    let responseText = await getTextFromStream(response.body)
+    document.getElementById('result').innerHTML = responseText;
 }
 
